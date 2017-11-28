5260c81f9333181cd505a71702f23e53d51b157e
CharlBest <charlbest@yahoo.com>
2017-11-28 12:16:07 +0200

    commit environment variables

--- Things.WebApp/src/environments/environment.prod.ts
+++ Things.WebApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  apiUrlEndpoint: 'https://things-prod-api.azurewebsites.net',
-  googleCustomSearchUrlEndpoint: '***',
-  stripePublishableKey: '***',
+  apiUrlEndpoint: 'https://things-cx-api.herokuapp.com/',
+  googleCustomSearchUrlEndpoint: 'https://www.googleapis.com/customsearch/v1?q=[SEARCH_TERM]&cx=005863448098847757980:hxez_lk5jy8&searchType=image&key=AIzaSyCZ0ok4aOSW31dz0TGa34Aegv-s-BrysCw&start=[PAGE_START]&fields=items%2Flink',
+  stripePublishableKey: 'pk_test_0Opnf68xnyT0RVyCQjqw0ScB',
   googleAnalytics: {
     web: 'UA-102909242-1',
     ios: 'UA-102909242-3',
     chromeExtension: 'UA-102909242-2'
   },
-  hostUrlForSharingToWeb: 'https://www.things.cx'
+  hostUrlForSharingToWeb: 'https://things.cx'
 };
--- Things.WebApp/src/environments/environment.ts
+++ Things.WebApp/src/environments/environment.ts
@@ -6,8 +6,8 @@
 export const environment = {
   production: false,
   apiUrlEndpoint: 'http://localhost:53305',
-  googleCustomSearchUrlEndpoint: '***',
-  stripePublishableKey: '***',
+  googleCustomSearchUrlEndpoint: 'https://www.googleapis.com/customsearch/v1?q=[SEARCH_TERM]&cx=005863448098847757980:hxez_lk5jy8&searchType=image&key=AIzaSyCZ0ok4aOSW31dz0TGa34Aegv-s-BrysCw&start=[PAGE_START]&fields=items%2Flink',
+  stripePublishableKey: 'pk_test_0Opnf68xnyT0RVyCQjqw0ScB',
   googleAnalytics: {
     web: 'UA-102909242-5',
     ios: 'UA-102909242-6',
